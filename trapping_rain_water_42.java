340621036b227a07f3b277dbbdf40dffffa7a6fa
Glen Jayson Dmello <120797275+glenjaysondmello@users.noreply.github.com>
2025-03-04 22:51:17 +0530

    Create trapping_rain_water_42.java

---
+++ trapping_rain_water_42.java
@@ -0,0 +1,27 @@
+class trapping_rain_water_42 {
+    public static int trap(int[] height) {
+        if(height.length == 0) return 0;
+        int n = height.length;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+        leftMax[0] = height[0];
+        rightMax[n-1] = height[n-1];
+        for(int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        }
+        for(int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i+1]);
+        }
+        int waterTrapped = 0;
+        for(int i = 0; i < n; i++) {
+            waterTrapped += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return waterTrapped;
+    }
+    public static void main(String args[]) {
+        int[] height1 = {0,1,0,2,1,0,1,3,2,1,2,1};
+        int[] height2 = {4,2,0,3,2,5};
+        System.out.println(trap(height1));
+        System.out.println(trap(height2));
+    }
+}
