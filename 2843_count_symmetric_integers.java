9f91e417c0476d9e591a5997ba90a97c8be0ad50
Glen Jayson Dmello <120797275+glenjaysondmello@users.noreply.github.com>
2025-04-11 23:43:20 +0530

    Update 2843_count_symmetric_integers.java

--- 2843_count_symmetric_integers.java
+++ 2843_count_symmetric_integers.java
@@ -1,16 +1,13 @@
-class 2843_count_symmetric_integers {
+class Solution {
     public static int countSymmetricIntegers(int low, int high) {
         int len, n, num1, num2, count = 0;
-        String s1, s2;
         for(int i = low; i <= high; i++) {
             String str = Integer.toString(i);
             len = str.length();
             if(len % 2 == 0) {
                 n = len / 2;
-                s1 = str.substring(0, n);
-                s2 = str.substring(n);
-                num1 = Integer.parseInt(s1);
-                num2 = Integer.parseInt(s2);
+                num1 = Integer.parseInt(str.substring(0, n));
+                num2 = Integer.parseInt(str.substring(n));
                 if(sumOfNum(num1) == sumOfNum(num2)) {
                     count++;
                 }
