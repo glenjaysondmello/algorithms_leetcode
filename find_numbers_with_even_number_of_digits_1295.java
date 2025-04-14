7e5e4e47c013fa1718705c9472eea0089a5ef507
Glen Jayson Dmello <120797275+glenjaysondmello@users.noreply.github.com>
2025-04-14 11:54:00 +0530

    Update find_numbers_with_even_number_of_digits_1295.java

--- find_numbers_with_even_number_of_digits_1295.java
+++ find_numbers_with_even_number_of_digits_1295.java
@@ -33,3 +33,23 @@ class find_numbers_with_even_number_of_digits_1295 {
         System.out.println(findNumbers(nums2));
     }
 }
+
+// Example 1:
+
+// Input: nums = [12,345,2,6,7896]
+// Output: 2
+// Explanation: 
+// 12 contains 2 digits (even number of digits). 
+// 345 contains 3 digits (odd number of digits). 
+// 2 contains 1 digit (odd number of digits). 
+// 6 contains 1 digit (odd number of digits). 
+// 7896 contains 4 digits (even number of digits). 
+// Therefore only 12 and 7896 contain an even number of digits.
+
+// Example 2:
+
+// Input: nums = [555,901,482,1771]
+// Output: 1 
+// Explanation: 
+// Only 1771 contains an even number of digits.
+ 
