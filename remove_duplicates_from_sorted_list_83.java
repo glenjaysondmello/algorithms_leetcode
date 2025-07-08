f6ab572017a793745ff36c1ca9f32521cf7f54b9
Glen Jayson Dmello <120797275+glenjaysondmello@users.noreply.github.com>
2025-07-08 10:51:35 +0530

    Create remove_duplicates_from_sorted_list_83.java

---
+++ remove_duplicates_from_sorted_list_83.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode node = head;
+
+        while(node != null && node.next != null) {
+            if(node.val == node.next.val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+
+        return head;
+    }
+}
+
+
+// Example 1:
+
+// Input: head = [1,1,2]
+// Output: [1,2]
+  
+// Example 2:
+
+// Input: head = [1,1,2,3,3]
+// Output: [1,2,3]
+
