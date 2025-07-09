3f00dad020ab466a4aac6ef922b0b9c64a0d8ab2
Glen Jayson Dmello <120797275+glenjaysondmello@users.noreply.github.com>
2025-07-09 20:12:07 +0530

    Update rotate_list_61.java

--- rotate_list_61.java
+++ rotate_list_61.java
@@ -9,8 +9,74 @@
  * }
  */
 
+// (Best Approach)
 
-// (Not Recommended)
+class rotate_list_61 {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null) return head;
+
+        int length = 1;
+        ListNode last = head;
+
+        while(last.next != null) {
+            last = last.next;
+            length++;
+        }
+
+        last.next = head;
+        int skip = length - (k % length); // length - rotations
+        ListNode newLast = head;
+
+        for(int i = 0; i < skip - 1; i++) {
+            newLast = newLast.next;
+        }
+
+        head = newLast.next;
+        newLast.next = null;
+
+        return head;
+    }
+}
+
+// (Improved version of below code by making computaion fast)
+
+class rotate_list_61 {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null) return head;
+
+        int length = getLength(head);
+        k %= length;
+
+        while(k-- > 0) {
+            ListNode prev = null;
+            ListNode node = head;
+
+            while(node.next != null) {
+                prev = node;
+                node = node.next;
+            }
+            
+            node.next = head;
+            head = node;
+            prev.next = null;
+        }
+
+        return head;
+    }
+
+    static int getLength(ListNode node) {
+        int length = 0;
+
+        while(node != null) {
+            node = node.next;
+            length++;
+        }
+
+        return length;
+    }
+}
+
+// (Not Recommended) Tinme limit exceeds
 
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
