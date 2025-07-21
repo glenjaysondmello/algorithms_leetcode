e42bdc003cf8f6b1d3520ece7cee15cef5c79355
Glen Jayson Dmello <120797275+glenjaysondmello@users.noreply.github.com>
2025-07-21 11:36:16 +0530

    Update populating_next_right_pointers_in_each_node_116.java

--- populating_next_right_pointers_in_each_node_116.java
+++ populating_next_right_pointers_in_each_node_116.java
@@ -21,6 +21,57 @@ class Node {
 };
 */
 
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+// Best Approach
+class populating_next_right_pointers_in_each_node_116 {
+    public Node connect(Node root) {
+        if(root == null) return null;
+
+        Node leftMost = root;
+
+        while(leftMost.left != null) {
+            Node currentNode = leftMost;
+
+            while(currentNode != null) {
+                currentNode.left.next = currentNode.right;
+
+                if(currentNode.next != null) {
+                    currentNode.right.next = currentNode.next.left;
+                }
+
+                currentNode = currentNode.next;
+            }
+
+            leftMost = leftMost.left;
+        }
+
+        return root;
+    }
+}
+
+// Using queue
 class populating_next_right_pointers_in_each_node_116 {
     public Node connect(Node root) {
         if(root == null) return null;
