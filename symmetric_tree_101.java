ba3eca99ff1e949e38ec79dad5398f00f27be51b
Glen Jayson Dmello <120797275+glenjaysondmello@users.noreply.github.com>
2025-07-22 12:29:16 +0530

    Create symmetric_tree_101.java

---
+++ symmetric_tree_101.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class symmetric_tree_101 {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return false;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root.left);
+        queue.offer(root.right);
+
+        while(!queue.isEmpty()) {
+            TreeNode leftNode = queue.poll(); 
+            TreeNode rightNode = queue.poll();
+
+            if(leftNode == null && rightNode == null) continue;
+            if(leftNode == null || rightNode == null) return false;
+            if(leftNode.val != rightNode.val) return false;
+
+            queue.offer(leftNode.left);
+            queue.offer(rightNode.right);
+            queue.offer(leftNode.right);
+            queue.offer(rightNode.left);
+        }
+
+        return true;
+    }
+}
+
+
+// Example 1:
+
+// Input: root = [1,2,2,3,4,4,3]
+// Output: true
+  
+// Example 2:
+
+// Input: root = [1,2,2,null,3,null,3]
+// Output: false
+
+  
