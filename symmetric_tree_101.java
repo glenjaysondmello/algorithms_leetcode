d1d06ede9abb6b4121767c2e06ce188d371cd4c9
Glen Jayson Dmello <120797275+glenjaysondmello@users.noreply.github.com>
2025-07-22 13:11:44 +0530

    Update symmetric_tree_101.java

--- symmetric_tree_101.java
+++ symmetric_tree_101.java
@@ -13,6 +13,25 @@
  *     }
  * }
  */
+
+// recursive (best approach)
+class symmetric_tree_101 {
+    public boolean isSymmetric(TreeNode root) {
+       if(root == null) return true;
+
+       return isMirror(root.left, root.right);
+    }
+
+    static boolean isMirror(TreeNode t1, TreeNode t2) {
+        if(t1 == null && t2 == null) return true;
+        if(t1 == null || t2 == null) return false;
+        if(t1.val != t2.val) return false;
+
+        return isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
+}
+
+// iterative (BFS)
 class symmetric_tree_101 {
     public boolean isSymmetric(TreeNode root) {
         if(root == null) return false;
