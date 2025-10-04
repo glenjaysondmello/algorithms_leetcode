7230d5ffe4b3bffc8da1515d73eed620d8e8198a
Glen Jayson Dmello <120797275+glenjaysondmello@users.noreply.github.com>
2025-10-04 15:11:31 +0530

    Update container_with_most_water_11.java

--- container_with_most_water_11.java
+++ container_with_most_water_11.java
@@ -1,3 +1,46 @@
+class container_with_most_water_11 {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+
+        while(left < right) {
+            int v1 = height[left];
+            int v2 = height[right];
+
+            int w = Math.min(v1, v2);
+            int l = right - left;
+
+            int area = w * l;
+
+            maxArea = Math.max(maxArea, area);
+
+            if(v1 < v2) {
+                left++;
+            } else if(v1 > v2) {
+                right--;
+            } else {
+                left++;
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
+
+// Example 1:
+
+// Input: height = [1,8,6,2,5,4,8,3,7]
+// Output: 49
+// Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+// Example 2:
+
+// Input: height = [1,1]
+// Output: 1
+
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------------
 import java.util.*;
 
 class container_with_most_water_11 {
