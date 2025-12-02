415838701b018c542a0a400b3eb1527ead154484
Glen Jayson Dmello <120797275+glenjaysondmello@users.noreply.github.com>
2025-12-02 20:10:04 +0530

    Enhance maxSubArray method with comments and examples
    Added comments and examples to clarify the implementation of Kadane's Algorithm for finding the maximum subarray sum.

---
+++ maximum_subarray_53.java
@@ -0,0 +1,33 @@
+// Kadane's Algorithm: Efficiently finds the maximum subarray sum in O(n) by tracking current and global maximums.
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int sum = nums[0];
+
+        for(int i = 1; i < nums.length; i++) {
+            sum = Math.max(nums[i], sum + nums[i]);
+            max = Math.max(max, sum);
+        }
+
+        return max;
+    }
+}
+
+// Example 1:
+
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+// Example 2:
+
+// Input: nums = [1]
+// Output: 1
+// Explanation: The subarray [1] has the largest sum 1.
+
+// Example 3:
+
+// Input: nums = [5,4,-1,7,8]
+// Output: 23
+// Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
