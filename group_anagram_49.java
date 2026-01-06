a45efa643d855b24af8ae363f219d153c0568aa5
Glen Jayson Dmello <120797275+glenjaysondmello@users.noreply.github.com>
2026-01-07 00:29:23 +0530

    Update group_anagram_49.java

--- group_anagram_49.java
+++ group_anagram_49.java
@@ -1,3 +1,41 @@
+// Use a HashMap with sorted characters as keys to group strings that are anagrams.
+class group_anagram_49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for(String word : strs) {
+            char[] charArr = word.toCharArray();
+            Arrays.sort(charArr);
+            String sortedArr = new String(charArr);
+
+            if(!map.containsKey(sortedArr)) {
+                map.put(sortedArr, new ArrayList<>());
+            }
+
+            map.get(sortedArr).add(word);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
+
+// Example 1:
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+// Explanation:
+// There is no string in strs that can be rearranged to form "bat".
+// The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+// The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+
+// Example 2:
+// Input: strs = [""]
+// Output: [[""]]
+
+// Example 3:
+// Input: strs = ["a"]
+// Output: [["a"]]
+
+
 import java.util.*;
 
 public class group_anagram_49 {
